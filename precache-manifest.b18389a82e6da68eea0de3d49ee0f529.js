cb83ac57f16000e9603eac0a01af80d0050f9ad6
tmnrwd <61465712+tmnrwd@users.noreply.github.com>
2020-09-16 22:38:08 +0100

    Updates

---
+++ precache-manifest.b18389a82e6da68eea0de3d49ee0f529.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "551f9d01c2b65da456148689c48fcf90",
+    "url": "/booklist/index.html"
+  },
+  {
+    "revision": "ff748cab90685ec3015b",
+    "url": "/booklist/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f839728a9ca63df71636",
+    "url": "/booklist/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ff748cab90685ec3015b",
+    "url": "/booklist/static/js/2.ca4f95ae.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/booklist/static/js/2.ca4f95ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f839728a9ca63df71636",
+    "url": "/booklist/static/js/main.7d8efcee.chunk.js"
+  },
+  {
+    "revision": "73483e3b23e0434b6320",
+    "url": "/booklist/static/js/runtime-main.9459ce74.js"
+  }
+]);
